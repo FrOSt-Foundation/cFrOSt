882f96d1ee04f87f1cf21bebbbd80168f2131540
Yamakaky <yamakaky@yamaworld.fr>
2016-06-29 19:21:33 +0200

    Add fixed size typedefs.

---
+++ include/types.h
@@ -0,0 +1,9 @@
+#pragma once
+
+typedef unsigned int u16;
+typedef int i16;
+typedef long i32;
+typedef unsigned long u32;
+typedef long long i64;
+typedef unsigned long long u64;
+typedef float f32;
