5f6bc78d5988177d319c6e1ab6008af86c024b33
azertyfun <azertyfun_poubelle@hotmail.fr>
2016-07-02 18:29:47 +0200

    Switched to use (=)m wherever possible in asm

--- src/drivers/hardware.c
+++ src/drivers/hardware.c
@@ -1,28 +1,25 @@
-#include "include/defines.h"
+#include "../include/types.h"
+#include "../include/defines.h"
 #include "hardware.h"
-#include "lem1802/lem1802.h"
 
 u16 monitor = 0xFFFF;
 u16 keyboard = 0xFFFF;
 u16 clock = 0xFFFF;
 
 void hardwareLoop() {
-    __asm volatile("hwn [0xF000]");
-    u16* p = (u16*) 0xF000;
-    u16 n_devices = *p;
+    u16 n_devices;
+    __asm volatile("hwn %0" : "=m" (n_devices));
 
-    u16 idA, idB;
     for(u16 device = 0; device < n_devices; ++device) {
-        *p = device;
+        u16 idA = 0, idB = 0;
         __asm volatile("set PUSH, A \n\t\
                         set PUSH, B \n\t\
-                        hwq [0xF000] \n\t\
-                        set [0xF000], A \n\t\
-                        set [0xF001], B \n\t\
+                        hwq %0 \n\t\
+                        set %1, A \n\t\
+                        set %2, B \n\t\
                         set B, POP \n\t\
-                        set A, POP");
-        idA = *p;
-        idB = *(p + 1);
+                        set A, POP"
+                    : "=m" (device) : "m" (idA), "m" (idB));
         if(idB == 0x7349 && idA == 0xf615) { // Monitor
             monitor = device;
             lem1802_init(monitor);
--- src/drivers/lem1802/lem1802.c
+++ src/drivers/lem1802/lem1802.c
@@ -4,15 +4,13 @@ u16 vram = 0x8000;
 u16 cursorPos = 0;
 
 void lem1802_init(u16 monitor) {
-    u16 *p = (u16 *) 0xF000;
-    *p = monitor;
     __asm volatile("set PUSH, A\n\t\
                     set PUSH, B\n\t\
                     set A, 0\n\t\
                     set B, 0x8000\n\t\
-                    hwi [0xF000]\n\t\
+                    hwi %0\n\t\
                     set B, POP\n\t\
-                    set A, POP");
+                    set A, POP" :: "m" (monitor));
 }
 
 void lem1802_puts(char* s) {
