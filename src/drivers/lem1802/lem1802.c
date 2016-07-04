1e6f7e5d45b46349e7a5e55721a64cf3f431f1c4
Yamakaky <yamakaky@yamaworld.fr>
2016-07-04 18:35:14 +0200

    Use a static array for vram

--- src/drivers/lem1802/lem1802.c
+++ src/drivers/lem1802/lem1802.c
@@ -4,10 +4,13 @@ typedef enum {
    MEM_MAP_SCREEN = 0,
 } lem1802_action ;
 
-u16 *vram = (u16*)0x8000;
-u16 cursorPos = 0;
+// Init to 1 to force clang not to use lcomm
+static u16 vram[386] = {1};
+static u16 cursorPos = 1;
 
 void lem1802_init(u16 monitor) {
+    cursorPos = 0;
+    vram[0] = 0;
     register u16 action __asm("A") = MEM_MAP_SCREEN;
     register u16 *vram_reg __asm("B") = vram;
     __asm("hwi %0"
--- src/drivers/lem1802/lem1802.h
+++ src/drivers/lem1802/lem1802.h
@@ -2,8 +2,5 @@
 
 #include "include/types.h"
 
-extern u16 *vram;
-extern u16 cursorPos;
-
 void lem1802_init(u16 monitor);
 void lem1802_puts(char* s);
