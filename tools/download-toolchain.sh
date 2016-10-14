97ac16c924b244b13650dfdfc2888c8b929d4f45
azertyfun <azertyfun_poubelle@hotmail.fr>
2016-10-14 17:01:01 +0200

    Updated DCPU-Toolchain to 1.4.3

--- tools/download-toolchain.sh
+++ tools/download-toolchain.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wget --no-verbose --show-progress --continue \
-    https://github.com/azertyfun/DCPU-Toolchain/releases/download/v1.4.2/DCPU-Toolchain-cli.zip \
+    https://github.com/azertyfun/DCPU-Toolchain/releases/download/v1.4.3/DCPU-Toolchain-cli.zip \
     https://github.com/Yamakaky/dcpu/releases/download/0.4.0/{assembler,emulator}
 unzip -o DCPU-Toolchain-cli.zip
 chmod +x {assembler,emulator}
