eb662dd6ebc90e4c15cfacd32e25979b9390b4ff
azertyfun <azertyfun_poubelle@hotmail.fr>
2016-10-19 20:26:17 +0200

    Added basic M35FD/M525HD support

--- src/drivers/mackapar/mackapar.c
+++ src/drivers/mackapar/mackapar.c
@@ -9,12 +9,12 @@ typedef enum {
     ACTION_SPIN_UP
 } Mackapar_action;
 
-void *mackapar_init_m35fd (u16 mackapar, u16 UNUSED(int_number), Int_handler *UNUSED(int_handler_location)) {
-    return mackapar_init(mackapar, MACKAPAR_M35FD);
+void *mackapar_init_m35fd (u16 mackapar, u16 UNUSED (int_number), Int_handler *UNUSED (int_handler_location)) {
+    return mackapar_init (mackapar, MACKAPAR_M35FD);
 }
 
-void *mackapar_init_m525hd (u16 mackapar, u16 UNUSED(int_number), Int_handler *UNUSED(int_handler_location)) {
-    return mackapar_init(mackapar, MACKAPAR_M525HD);
+void *mackapar_init_m525hd (u16 mackapar, u16 UNUSED (int_number), Int_handler *UNUSED (int_handler_location)) {
+    return mackapar_init (mackapar, MACKAPAR_M525HD);
 }
 
 void *mackapar_init (u16 mackapar, Mackapar_type type) {
@@ -22,34 +22,33 @@ void *mackapar_init (u16 mackapar, Mackapar_type type) {
     data->mackapar = mackapar;
     data->type = type;
 
-    mackapar_spin_up(data); // No need to check the type, at worst this function doesn't do anything
+    mackapar_spin_up (data); // No need to check the type, at worst this function doesn't do anything
 
     return data;
 }
 
-void mackapar_destroy (void* UNUSED(data)) {
-
+void mackapar_destroy (void *UNUSED (data)) {
 }
 
 u16 mackapar_update_function (void *data, u16 message, u16 arg1, u16 arg2) {
-    switch((Mackapar_message) message) {
+    switch ((Mackapar_message)message) {
         case MACKAPAR_POLL_DEVICE:
-            mackapar_poll_device(data, (u16*) arg1, (u16*) arg2);
+            mackapar_poll_device (data, (u16 *)arg1, (u16 *)arg2);
             break;
         case MACKAPAR_SET_INTERRUPT:
-            mackapar_set_interrupt(data, arg1);
+            mackapar_set_interrupt (data, arg1);
             break;
         case MACKAPAR_READ_SECTOR:
-            mackapar_read_sector(data, arg1, (void*) arg2);
+            mackapar_read_sector (data, arg1, (void *)arg2);
             break;
         case MACKAPAR_WRITE_SECTOR:
-            mackapar_write_sector(data, arg1, (void*) arg2);
+            mackapar_write_sector (data, arg1, (void *)arg2);
             break;
         case MACKAPAR_SPIN_DOWN:
-            mackapar_spin_down(data);
+            mackapar_spin_down (data);
             break;
         case MACKAPAR_SPIN_UP:
-            mackapar_spin_up(data);
+            mackapar_spin_up (data);
             break;
     }
 
@@ -58,10 +57,10 @@ u16 mackapar_update_function (void *data, u16 message, u16 arg1, u16 arg2) {
 
 // TODO
 
-void mackapar_poll_device(Mackapar_driver_data* data, u16* state, u16* error) {
+void mackapar_poll_device (Mackapar_driver_data *data, u16 *state, u16 *error) {
     register u16 action __asm("A") = ACTION_POLL_DEVICE;
-    register u16* arg1 __asm("X") = state;
-    register u16* arg2 __asm("X") = error;
+    register u16 *arg1 __asm("X") = state;
+    register u16 *arg2 __asm("X") = error;
     __asm volatile("hwi %2"
                    : "=r"(arg1),
                      "=r"(arg2)
@@ -69,7 +68,7 @@ void mackapar_poll_device(Mackapar_driver_data* data, u16* state, u16* error) {
                      "r"(action));
 }
 
-void mackapar_set_interrupt(Mackapar_driver_data* data, u16 interrupt) {
+void mackapar_set_interrupt (Mackapar_driver_data *data, u16 interrupt) {
     register u16 action __asm("A") = ACTION_SET_INTERRUPT;
     register u16 arg1 __asm("X") = interrupt;
     __asm volatile("hwi %0"
@@ -79,10 +78,10 @@ void mackapar_set_interrupt(Mackapar_driver_data* data, u16 interrupt) {
                      "r"(arg1));
 }
 
-void mackapar_read_sector(Mackapar_driver_data* data, u16 sector, void* buffer) {
+void mackapar_read_sector (Mackapar_driver_data *data, u16 sector, void *buffer) {
     register u16 action __asm("A") = ACTION_READ_SECTOR;
     register u16 arg1 __asm("X") = sector;
-    register void* arg2 __asm("Y") = buffer;
+    register void *arg2 __asm("Y") = buffer;
     __asm volatile("hwi %1"
                    : "=r"(arg2)
                    : "X"(data->mackapar),
@@ -90,10 +89,10 @@ void mackapar_read_sector(Mackapar_driver_data* data, u16 sector, void* buffer) 
                      "r"(arg1));
 }
 
-void mackapar_write_sector(Mackapar_driver_data* data, u16 sector, void* buffer) {
+void mackapar_write_sector (Mackapar_driver_data *data, u16 sector, void *buffer) {
     register u16 action __asm("A") = ACTION_WRITE_SECTOR;
     register u16 arg1 __asm("X") = sector;
-    register void* arg2 __asm("Y") = buffer;
+    register void *arg2 __asm("Y") = buffer;
     __asm volatile("hwi %1"
                    : "=r"(arg2)
                    : "X"(data->mackapar),
@@ -101,8 +100,8 @@ void mackapar_write_sector(Mackapar_driver_data* data, u16 sector, void* buffer)
                      "r"(arg1));
 }
 
-void mackapar_spin_down(Mackapar_driver_data* data) {
-    if(data->type == MACKAPAR_M525HD) {
+void mackapar_spin_down (Mackapar_driver_data *data) {
+    if (data->type == MACKAPAR_M525HD) {
         register u16 action __asm("A") = ACTION_SPIN_DOWN;
         __asm volatile("hwi %0"
                        :
@@ -111,8 +110,8 @@ void mackapar_spin_down(Mackapar_driver_data* data) {
     }
 }
 
-void mackapar_spin_up(Mackapar_driver_data* data) {
-    if(data->type == MACKAPAR_M525HD) {
+void mackapar_spin_up (Mackapar_driver_data *data) {
+    if (data->type == MACKAPAR_M525HD) {
         register u16 action __asm("A") = ACTION_SPIN_UP;
         __asm volatile("hwi %0"
                        :
--- src/drivers/mackapar/mackapar.h
+++ src/drivers/mackapar/mackapar.h
@@ -21,15 +21,15 @@ typedef struct {
 } Mackapar_driver_data;
 
 
-void *mackapar_init_m35fd (u16 mackapar, u16 UNUSED(int_number), Int_handler *UNUSED(int_handler_location));
-void *mackapar_init_m525hd (u16 mackapar, u16 UNUSED(int_number), Int_handler *UNUSED(int_handler_location));
+void *mackapar_init_m35fd (u16 mackapar, u16 UNUSED (int_number), Int_handler *UNUSED (int_handler_location));
+void *mackapar_init_m525hd (u16 mackapar, u16 UNUSED (int_number), Int_handler *UNUSED (int_handler_location));
 void *mackapar_init (u16 mackapar, Mackapar_type type);
-void mackapar_destroy (void* UNUSED(data));
+void mackapar_destroy (void *UNUSED (data));
 u16 mackapar_update_function (void *data, u16 message, u16 arg1, u16 arg2);
 
-void mackapar_poll_device(Mackapar_driver_data* data, u16* state, u16* error);
-void mackapar_set_interrupt(Mackapar_driver_data* data, u16 interrupt);
-void mackapar_read_sector(Mackapar_driver_data* data, u16 sector, void* buffer);
-void mackapar_write_sector(Mackapar_driver_data* data, u16 sector, void* buffer);
-void mackapar_spin_down(Mackapar_driver_data* data);
-void mackapar_spin_up(Mackapar_driver_data* data);
+void mackapar_poll_device (Mackapar_driver_data *data, u16 *state, u16 *error);
+void mackapar_set_interrupt (Mackapar_driver_data *data, u16 interrupt);
+void mackapar_read_sector (Mackapar_driver_data *data, u16 sector, void *buffer);
+void mackapar_write_sector (Mackapar_driver_data *data, u16 sector, void *buffer);
+void mackapar_spin_down (Mackapar_driver_data *data);
+void mackapar_spin_up (Mackapar_driver_data *data);
