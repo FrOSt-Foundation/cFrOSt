00cb1d21c43f0e423e0c208fbb7ffbe031ddca8f
L3nn0x <dragon.super@laposte.net>
2017-01-14 21:37:42 +0100

    panic function should work now

--- src/kernel/panic/panic.c
+++ src/kernel/panic/panic.c
@@ -1,8 +1,9 @@
 #include "panic.h"
+#include "asm.h"
 
 __attribute__ ((noreturn)) void kpanic (char *data) {
     asm_log ((u16)data);
     asm_brk (0);
     asm_iaq (1);
-    asm_hlt();
+    while (1); // should never go there
 }
